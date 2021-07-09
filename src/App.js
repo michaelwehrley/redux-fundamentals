c3325bea9b8420e274054ea651edf1b86a35049d
Michael Wehrley <michaelwehrley@users.noreply.github.com>
2021-07-09 16:10:06 -0500

    add enhancer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
 import './App.css';
+import {
+  createStore,
+  compose,
+  applyMiddleware,
+  bindActionCreators
+} from "redux";
+
+const reducer = state => state;
+
+const monitorEnhancer = (createStore) => (reducer, initialState, enhancer) => {
+  const monitorReducer = (state, action) => {
+    const start = performance.now();
+    const newState = reducer(state, action);
+    const end = performance.now();
+    const diff = Math.round(end - start);
+    console.log(diff);
+
+    return newState;
+  }
+
+  return createStore(monitorReducer, initialState, enhancer);
+};
+
+const store = createStore(reducer, monitorEnhancer);
+
+store.dispatch({type: "Hello"})
 
 function App() {
   return (
